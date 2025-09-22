1921b0957ba036f990286ae99b9b11bb2f0a012d
mateussperess <mateuspereslopesl@outlook.com>
2025-09-21 21:59:22 -0300

    ajustando mudança de fotos de acordo com o IMC

--- app/src/main/java/com/example/appimc/MainActivity.java
+++ app/src/main/java/com/example/appimc/MainActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     public EditText etHeight;
     public TextView txtResult;
     public Button btnResult;
+    public ImageView imgResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         etWeight = findViewById(R.id.etWeight);
         etHeight = findViewById(R.id.etHeight);
         txtResult = findViewById(R.id.txtResult);
+        imgResult = findViewById(R.id.imgResult);
         btnResult = findViewById(R.id.btnResult);
 
         btnResult.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +44,27 @@ public class MainActivity extends AppCompatActivity {
                 float weight = Float.parseFloat(etWeight.getText().toString());
                 float height = Float.parseFloat(etHeight.getText().toString());
                 float result = weight / (height * height);
+
+                if(result < 18.5) {
+                    Log.d("CALCULO", "Abaixo do peso");
+                    imgResult.setImageResource(R.drawable.abaixo_peso);
+                } else if (result >= 18.5 && result < 25) {
+                    Log.d("CALCULO", "Peso normal");
+                    imgResult.setImageResource(R.drawable.peso_normal);
+                } else if (result >= 25 && result < 30) {
+                    Log.d("CALCULO", "Sobrepeso");
+                    imgResult.setImageResource(R.drawable.sobrepeso);
+                } else if (result >= 30 && result < 35) {
+                    Log.d("CALCULO", "Obesidade I");
+                    imgResult.setImageResource(R.drawable.obesidade1);
+                } else if (result >= 35 && result < 40) {
+                    Log.d("CALCULO", "Obesidade II");
+                    imgResult.setImageResource(R.drawable.obesidade2);
+                } else {
+                    Log.d("CALCULO", "Obesidade mórbida");
+                    imgResult.setImageResource(R.drawable.obesidade_morbida);
+                }
+
                 txtResult.setText("IMC: " + result);
             }
         });
